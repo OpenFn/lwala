fd0504bdb863ce438672db1723c6ae428e7541ed
willedman <wce5@georgetown.edu>
2017-08-28 12:37:21 +0300

    Removed visits for debugging

--- commcare-salesforce-jobs/Update-Person-in-SF-Production.js
+++ commcare-salesforce-jobs/Update-Person-in-SF-Production.js
@@ -751,13 +751,13 @@ combine(function(state){
     )(state);
   }
 }),
-create("Visit__c",fields(
+/*create("Visit__c",fields(
   relationship("Household__r","CommCare_Code__c",dataValue("$.form.HH_ID")),
   field("Household_CHW__c",dataValue("$.form.CHW_ID_Final")),
   field("Date__c",dataValue("$.metadata.timeEnd"))
   //field("Location__latitude__s",dataValue("$.metadata.location[0]")),
   //field("Location__longitude__s",dataValue("$.metadata.location[1]"))
-))
+))*/
 
 
   
