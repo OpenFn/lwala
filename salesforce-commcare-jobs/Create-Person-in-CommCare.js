020c4612e3d9aa548ef532f06fa0ce83c252f6f8
Will Edman <wce5@georgetown.edu>
2017-09-29 12:55:17 -0700

    same as above

--- salesforce-commcare-jobs/Create-Person-in-CommCare.js
+++ salesforce-commcare-jobs/Create-Person-in-CommCare.js
@@ -129,7 +129,7 @@ submit(
             "n0:case_name": dataValue("new[0].Name")(state),
             "n0:owner_id": function(){
               var id="";
-               if(dataValue("new[0].chw_owner_id__c")(state)===undefined && dataValue("chw")(state)===undefined){
+               if((dataValue("new[0].chw_owner_id__c")(state)===undefined && dataValue("chw")(state)===undefined)||dataValue("chw")(state)=="null"){
                  id="acf8595692c76095eb5afd809c628091";
                }
                else if(dataValue("new[0].chw_owner_id__c")(state)===undefined){
