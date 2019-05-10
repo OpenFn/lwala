8176314231475213de870045275cbe86576244e9
Taylor Downs <taylor@openfn.org>
2019-05-10 03:27:11 +0100

    will@lwalacommunityalliance.org's change on OpenFn.org

--- salesforce-commcare-jobs/Close-Person-Case.js
+++ salesforce-commcare-jobs/Close-Person-Case.js
@@ -6,14 +6,14 @@ submit(
         "xmlns":"http://openrosa.org/formdesigner/980c10cdb3b140101225e25c6e8aff48f471b3d",
         "uiVersion":"1",
         "version":"46",
-        "name":"Update Household"
+        "name":"Update Person"
       };
     }),
     
     field("n0:case", function(state){
       return{
         "@": {
-            "case_id": dataValue("new[0].Commcare_Code__c")(state),
+            "case_id": dataValue("new[0].Commcare_ID__c")(state),
             "date_modified": new Date().toISOString(),
             "user_id": "e298884bfb6ee2d2b38591a6e8ae0228",
             "xmlns:n0": "http://commcarehq.org/case/transaction/v2"
