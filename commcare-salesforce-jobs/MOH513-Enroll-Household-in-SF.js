1571a413e83625fed2ee7fecee3f0b35a855f12b
aleksa-krolls <aleksagk@gmail.com>
2019-10-22 13:09:32 +0200

    updating person HH mapping again

--- commcare-salesforce-jobs/MOH513-Enroll-Household-in-SF.js
+++ commcare-salesforce-jobs/MOH513-Enroll-Household-in-SF.js
@@ -74,7 +74,7 @@ each(
   upsert("Person__c","CommCare_ID__c", fields(
     //relationship("Household__r", "CommCare_Code__c", dataValue("case.index.parent.#text")),
     field("CommCare_ID__c",dataValue("case.@case_id")),
-    field("CommCare_HH_Code__c", state.data.form.case.@case_id),
+    field("CommCare_HH_Code__c", dataValue("case.index.parent.#text")),
     relationship("RecordType","Name",(state)=>{
       var rt = dataValue("Basic_Information.Record_Type")(state)
       var status = dataValue("Basic_Information.Child_Status")(state)
