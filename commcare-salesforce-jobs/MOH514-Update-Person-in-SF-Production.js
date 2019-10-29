a1d297d3db66c624597902409a44b610b7f3fcdb
aleksa-krolls <aleksagk@gmail.com>
2019-10-29 09:39:38 +0200

    updated field mappings for new fields

--- commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
+++ commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
@@ -95,12 +95,12 @@ steps(
       field("Diarrhoea_less_than_14_days__c",dataValue("form.treatment_and_tracking.mild_symptoms_check_diarrhea")),
       field("Default_on_TB_treatment__c",dataValue("form.treatment_and_tracking.patient_default_tb")),
       field("TB_patients_therapy_observed__c",dataValue("form.treatment_and_tracking.observed_tb_therapy")),
-      field("Injuries_and_wounds_managed__c", dataValue("Injuries_and_wounds_managed")),
-      field("Injuries_or_wounds__c", dataValue("form.case.update.Injuries_or_wounds")),
-      field("Currently_on_ART_s__c", dataValue("form.case.update.ART")),
-      field("ART_Regimen__c", dataValue("form.case.update.ARVs")),
-      field("Immediate_Breastfeeding__c", dataValue("form.case.update.Immediate_Breastfeeding")),
-      field("Verbal_autopsy__c", dataValue("form.case.update.verbal_autopsy")),
+      //field("Injuries_and_wounds_managed__c", dataValue("Injuries_and_wounds_managed")),
+      field("Injuries_or_wounds__c", dataValue("form.treatment_and_tracking.wounds_or_injuries")),
+      field("Currently_on_ART_s__c", dataValue("form.HAWI.ART")),
+      field("ART_Regimen__c", dataValue("form.HAWI.ARVs")),
+      field("Immediate_Breastfeeding__c", dataValue("form.ANCs.pregnancy_danger_signs.Delivery_Information.Breastfeeding_Delivery")),
+      field("Verbal_autopsy__c", dataValue("form.Status.verbal_autopsy")),
       field("Date_of_Birth__c",dataValue("form.case.update.DOB")),
       field("Place_of_Delivery__c",(state)=>{
         var facility=dataValue("form.TT5.Child_Information.Delivery_Type")(state);
@@ -109,7 +109,10 @@ steps(
           }
         return facility;
       }),
-      field("Delivery_Facility__c",dataValue("form.case.update.Delivery_Facility")),
+      field("Delivery_Facility__c",(state)=>{
+        var facility= dataValue("form.ANCs.pregnancy_danger_signs.Delivery_Information.Delivery_Facility")(state);
+        return (facility!==undefined ? facility.toString().replace(/_/g," ") : null)
+      }),
       field("Exclusive_Breastfeeding__c",dataValue("form.TT5.Child_Information.Exclusive_Breastfeeding.Exclusive_Breastfeeding")),
       field("Counselled_on_Exclusive_Breastfeeding__c",dataValue("form.TT5.Child_Information.Exclusive_Breastfeeding.counseling")),
       field("Family_Planning__c", (state)=>{
@@ -218,7 +221,7 @@ steps(
         var name2=name1.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
         return name2;
       }),
-      field("Verbal_autopsy__c", dataValue("form.case.update.verbal_autopsy")),
+      field("Verbal_autopsy__c", dataValue("form.Status.verbal_autopsy")),
       field("Client_Status__c","Deceased"),
       field("Active_in_Thrive_Thru_5__c","No"),
       field("Active_in_HAWI__c","No"),
