448c9b40fb6aa7d8f4affca556b45eb44e7105f2
aleksa-krolls <aleksagk@gmail.com>
2020-05-20 07:26:57 -0700

    handle undefined locations

--- commcare-salesforce-jobs/MOH513-Update-Household-in-SF-Production.js
+++ commcare-salesforce-jobs/MOH513-Update-Household-in-SF-Production.js
@@ -60,12 +60,12 @@ upsert("Visit__c", "CommCare_Visit_ID__c", fields(
   }),
   field("Location__latitude__s", (state)=>{
     var lat = state.data.metadata.location[`#text`];
-    lat = lat.substring(0, lat.indexOf(" "));
-    return (lat!==null? lat : null);
+    //lat = lat.substring(0, lat.indexOf(" "));
+    return (lat!==null? lat.substring(0, lat.indexOf(" ")) : null);
   }),
  field("Location__longitude__s", (state)=>{
     var long = state.data.metadata.location[`#text`];
-    long = long.substring(long.indexOf(" ")+1, long.indexOf(" ")+7);
-    return (long!==null? long : null);
+    //long = long.substring(long.indexOf(" ")+1, long.indexOf(" ")+7);
+    return (long!==null? long.substring(long.indexOf(" ")+1, long.indexOf(" ")+7) : null);
   })
 ));
