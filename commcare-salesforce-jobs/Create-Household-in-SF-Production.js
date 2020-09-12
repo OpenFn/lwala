2c0d835aa9f5459ca2dee5d4d23197b8ad14afba
Aleksa Krolls <aleksagk@gmail.com>
2020-09-12 12:08:44 +0300

    aleksa@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/Create-Household-in-SF-Production.js
+++ commcare-salesforce-jobs/Create-Household-in-SF-Production.js
@@ -18,7 +18,7 @@ upsert("Household__c", "CommCare_Code__c", fields(
     }
   }),
   field("CommCare_Code__c",dataValue("$.form.case.@case_id")),
-  field("Household_CHW__c",dataValue("$.form.CHW_ID")),
+  //field("Household_CHW__c",dataValue("$.form.CHW_ID")),
   field("Area__c",dataValue("$.form.area")),
   field("Household_Head_No_Program__c",dataValue("form.No_Program_Head")),
   field("Treats_Drinking_Water__c",dataValue("$.form.Household_Information.Treats_Drinking_Water")),
