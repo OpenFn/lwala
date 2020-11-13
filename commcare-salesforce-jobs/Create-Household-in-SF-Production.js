861329856ddd9a47b2d02d2cb6ba635349e55fe5
Aleksa Krolls <aleksagk@gmail.com>
2020-11-13 14:01:16 +0200

    aleksa@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/Create-Household-in-SF-Production.js
+++ commcare-salesforce-jobs/Create-Household-in-SF-Production.js
@@ -11,7 +11,7 @@ upsert(
   'Household__c',
   'CommCare_Code__c',
   fields(
-    field('Name', 'test1'),
+    field('Name', dataValue('$.form.case.@case_id')),
     field('Catchment__c', state => {
       if (dataValue('form.catchment')(state) == 'East Kamagambo') {
         return 'a002400000pAcQt';
