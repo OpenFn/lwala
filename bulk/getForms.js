bd23d10dda374b3d397480a4ad42313ee160ec15
Aleksa Krolls <aleksagk@gmail.com>
2020-12-03 03:20:29 +0200

    aleksa@openfn.org's change on OpenFn.org

--- bulk/getForms.js
+++ bulk/getForms.js
@@ -1,5 +1,5 @@
 get(
-  'https://www.commcarehq.org/a/lwala-community-alliance/api/v0.5/form/798f5a9f-a116-4f63-8d2e-e59f3dab3feb',
+  'https://www.commcarehq.org/a/lwala-community-alliance/api/v0.5/form/',
   {
     query: {
       limit: 1000, //max limit
@@ -9,9 +9,9 @@ get(
           : 0,
       // Filter by Update Person form
       //xmlns: 'http://openrosa.org/formdesigner/25cb9620c678f403ccb59839cc22f91bd1c5d134',
-      /*xmlns: 'http://openrosa.org/formdesigner/ecca8b571aa3cdb7ed2df493301d0e885211d09a',
+      xmlns: 'http://openrosa.org/formdesigner/ecca8b571aa3cdb7ed2df493301d0e885211d09a',
       received_on_start: '2018-01-01',
-      received_on_end: '2018-10-30'*/
+      received_on_end: '2018-10-30'
     },
   },
   state => {
