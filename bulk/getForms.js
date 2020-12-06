6da472dc7172e2af663bfc29614e7f26302195bf
Aleksa Krolls <aleksagk@gmail.com>
2020-12-06 19:17:06 +0200

    aleksa@openfn.org's change on OpenFn.org

--- bulk/getForms.js
+++ bulk/getForms.js
@@ -8,10 +8,10 @@ get(
           ? state.meta.limit + state.meta.offset
           : 0,
       // Filter by Update Person form
-      xmlns: 'http://openrosa.org/formdesigner/25cb9620c678f403ccb59839cc22f91bd1c5d134',
-      //xmlns: 'http://openrosa.org/formdesigner/ecca8b571aa3cdb7ed2df493301d0e885211d09a',
-      received_on_start: '2018-11-01',
-      received_on_end: '2018-12-31'
+      //xmlns: 'http://openrosa.org/formdesigner/25cb9620c678f403ccb59839cc22f91bd1c5d134',
+      xmlns: 'http://openrosa.org/formdesigner/ecca8b571aa3cdb7ed2df493301d0e885211d09a',
+      received_on_start: '2019-01-01',
+      received_on_end: '2019-12-31'
       /*received_on_start: '2018-11-01',
       received_on_end: '2018-12-31'*/
     },
