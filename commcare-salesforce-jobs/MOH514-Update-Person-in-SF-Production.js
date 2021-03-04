17ab4031cb159f9de4fdaf26957c9745354720a6
Aleksa Krolls <aleksagk@gmail.com>
2021-03-04 07:33:48 +0200

    aleksa@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
+++ commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
@@ -338,7 +338,7 @@ alterState(state => {
         field('Counselled_on_FP_Methods__c', dataValue('form.TT5.Mother_Information.CounselledFP_methods')),
         field('Client_counselled_on__c', state => {
           var choice = dataValue('form.treatment_and_tracking.counseling.counsel_topic')(state);
-          return state.counselMap(choice); 
+          return state.counselMap[choice]; 
         }),
         field('woman_15_49yrs__c', dataValue('form.TT5.Mother_Information.was_the_woman_15-49yrs_provided_with_family_planning_commodities_by_chv')),
         field('Newborn_visited_48_hours_of_delivery__c', dataValue('form.TT5.Child_Information.newborn_visited_48_hours_of_delivery')),
