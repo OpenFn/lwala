3f11e79ba3fa219e135e71428311c37ae6d2d011
Aleksa Krolls <aleksagk@gmail.com>
2021-04-28 12:14:13 +0300

    aleksa@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/MOH513-Enroll-Person-in-SF.js
+++ commcare-salesforce-jobs/MOH513-Enroll-Person-in-SF.js
@@ -401,7 +401,7 @@ alterState(state => {
             };
             return reason ? reasonsMap[reason] : reason;
           }),
-        field("No_Preg_Test", (state) => {
+        field("Reason_for_not_taking_a_pregnancy_test__c", (state) => {
           var reason = dataValue("form.Person.asic_Information.family_planning.No_Preg_Test")(state);
           return reason
             ? reason.toString().replace(/_/g, " ")
