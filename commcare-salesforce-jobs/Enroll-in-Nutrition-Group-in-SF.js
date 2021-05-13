a2c06ca807a0ec085150adc12ac01791150ec766
Aleksa Krolls <aleksagk@gmail.com>
2021-05-13 13:15:26 +0300

    aleksa@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/Enroll-in-Nutrition-Group-in-SF.js
+++ commcare-salesforce-jobs/Enroll-in-Nutrition-Group-in-SF.js
@@ -7,7 +7,6 @@ create(
       dataValue('$.form.case.@case_id')
     ),
     field('Group__c', dataValue('$.form.sfid')),
-    //relationship("Group__c","Name",dataValue("$.form.Nutrition_Group")),
     field('Start_Date__c', dataValue('form.meta.timeEnd'))
   )
 );
