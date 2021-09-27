ef44108821db578d3b515e43493c149edd777bfc
Aleksa Krolls <aleksagk@gmail.com>
2021-09-27 17:47:08 +0300

    Create Update-HH-Name.js

---
+++ salesforce-commcare-jobs/Update-HH-Name.js
@@ -0,0 +1,11 @@
+//Update CommCare case
+submitXls(
+   [
+     {case_id: 'xxxx', name: '000000'},
+   ],
+   {
+     case_type: 'household',
+     search_field: 'case_id',
+     create_new_cases: 'off',
+   }
+)
