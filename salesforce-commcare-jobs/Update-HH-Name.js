da1e0657b6024cbad26291735e5d2bd12fe76212
Aleksa Krolls <aleksagk@gmail.com>
2021-09-27 18:05:30 +0300

    aleksa@openfn.org's change on OpenFn.org

--- salesforce-commcare-jobs/Update-HH-Name.js
+++ salesforce-commcare-jobs/Update-HH-Name.js
@@ -2,13 +2,14 @@
 submitXls(
    [
      {
-       case_id: dataValue('Envelope.Body.notifications.Notification.sObject.Commcare_Code__c'), 
-       name: dataValue('Envelope.Body.notifications.Notification.sObject.Household_Code_Autonumber__c')
+       case_id: state.data.Envelope.Body.notifications.Notification.sObject.Commcare_Code__c, 
+       name: state.data.Envelope.Body.notifications.Notification.sObject.Household_Code_Autonumber__c
      },
    ],
    {
      case_type: 'household',
      search_field: 'case_id',
+     search_column: 'case_id',
      create_new_cases: 'off',
    }
 )
