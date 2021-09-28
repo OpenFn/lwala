055aa5d9087ef15b94904f34de60f58c0a32e1af
Taylor Downs <taylor@openfn.org>
2021-09-28 12:42:32 +0100

    elias@openfn.org's change on OpenFn.org

--- salesforce-commcare-jobs/Update-HH-Name.js
+++ salesforce-commcare-jobs/Update-HH-Name.js
@@ -1,24 +1,26 @@
 //Update CommCare case
 alterState(state => {
   console.log(`Mapping HH code to CommCare: `, dataValue('Envelope.Body.notifications.Notification.sObject.Household_Code_Autonumber__c')(state));
-  console.log("State")
-  const notifications = Array.isArray(state.data.Envelope.Body.notifications) ? state.data.Envelope.Body.notifications : [state.data.Envelope.Body.notifications]
   
-  return { ...state, notifications}; 
+  const notifications = Array.isArray(state.data.Envelope.Body.notifications) ? state.data.Envelope.Body.notifications : [state.data.Envelope.Body.notifications]
+  return { ...state, notifications, values: []}; 
   
 }); 
-submitXls(
-  [
-    {
-      case_id: dataValue('Envelope.Body.notifications.Notification.sObject.Commcare_Code__c')(state), 
-      name: dataValue('Envelope.Body.notifications.Notification.sObject.Household_Code_Autonumber__c')(state)
-    },
-  ],
-  {
-    case_type: 'Household',
-    search_field: 'case_id',
-    search_column: 'case_id',
-    name_column: 'name',
-    create_new_cases: 'off',
+
+each("$.notifications[*]", state => {
+  const value = {
+    case_id: dataValue('Notification.sObject.Commcare_Code__c')(state), 
+    name: dataValue('Notification.sObject.Household_Code_Autonumber__c')(state)
   }
-)
+  state.values.push(value)
+  return state
+})
+
+
+submitXls(state => state.values, {
+  case_type: 'Household',
+  search_field: 'case_id',
+  search_column: 'case_id',
+  name_column: 'name',
+  create_new_cases: 'off',
+})
