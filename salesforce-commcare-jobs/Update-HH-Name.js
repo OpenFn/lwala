d87d88c41265089fd1023e6087a744c5a9cb9c19
Taylor Downs <taylor@openfn.org>
2021-09-28 12:20:17 +0100

    elias@openfn.org's change on OpenFn.org

--- salesforce-commcare-jobs/Update-HH-Name.js
+++ salesforce-commcare-jobs/Update-HH-Name.js
@@ -3,10 +3,7 @@ alterState(state => {
   console.log(`Mapping HH code to CommCare: `, dataValue('Envelope.Body.notifications.Notification.sObject.Household_Code_Autonumber__c')(state));
   console.log("State")
   const notifications = state.data.Envelope.Body.notifications
-  if (!Array.isArray(notifications)) {
-    return { ...state, [notifications]}
-  }
-  return state; 
+  return { ...state,  Array.isArray(notifications) ? notifications : [notifications] }; 
   
 }); 
 submitXls(
