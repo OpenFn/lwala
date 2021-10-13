a0e98542d48d3d545672ec41a73d3598620f2f27
Taylor Downs <taylor@openfn.org>
2021-10-13 15:08:16 +0100

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- commcare-salesforce-jobs/TEST-MOH514-Update-Person-in-SF-Production.js
+++ commcare-salesforce-jobs/TEST-MOH514-Update-Person-in-SF-Production.js
@@ -3124,3 +3124,39 @@ alterState((state) => {
     console.log("No Family Planning referral.");
     return state;  
 });
+//Chronic Illness
+alterState((state) => {
+  if (
+    dataValue("form.Person/Basic_Information/person_info/refer_chronic_illness")(state) == "yes"
+  ) {
+    return upsert(
+      "Service__c",
+      "Service_UID__c",
+      fields(
+        field("CommCare_Code__c", (state) => {
+          var id = dataValue("id")(state);
+          var serviceId = id + "chronic_illness";
+          return serviceId;
+        }),
+        field("Service_UID__c", (state) => {
+          var id = dataValue("id")(state);
+          var serviceId = id + "chronic_illness";
+          return serviceId;
+        }),
+        field("Household_CHW__c", dataValue("form.CHW_ID_Final"),
+          field("Type_of_Service__c", "Illness"),
+          field("Reason_for_Service__c", "Chronic Illness"),
+          field("Date__c", dataValue("form.case.update.Date"),
+            field("RecordTypeID", "01224000000YAuK"),
+            relationship(
+              "Person__r",
+              "CommCare_ID__c",
+              dataValue("form.case.@case_id")
+            ),
+          )
+        )
+      ))(state);
+    }
+    console.log("No chronic illness referral.");
+    return state;  
+});
