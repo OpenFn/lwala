51d5ad8c6c4c5e9cd0a3e1ebdcf25396205f2f58
Taylor Downs <taylor@openfn.org>
2021-10-13 15:18:59 +0100

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
+++ commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
@@ -3201,5 +3201,41 @@ alterState((state) => {
     console.log("No HIV HTC referral.");
     return state;  
 });
+//HIV - Clinical
+alterState((state) => {
+  if (
+    dataValue("form.Person/HAWI/did_you_refer")(state) == "yes"
+  ) {
+    return upsert(
+      "Service__c",
+      "Service_UID__c",
+      fields(
+        field("CommCare_Code__c", (state) => {
+          var id = dataValue("id")(state);
+          var serviceId = id + "HIV_Clinical";
+          return serviceId;
+        }),
+        field("Service_UID__c", (state) => {
+          var id = dataValue("id")(state);
+          var serviceId = id + "HIV_Clinical";
+          return serviceId;
+        }),
+        field("Household_CHW__c", dataValue("form.CHW_ID_Final"),
+          field("Type_of_Service__c", "HIV"),
+          field("Reason_for_Service__c", "ART treatment"),
+          field("Date__c", dataValue("form.case.update.Date"),
+            field("RecordTypeID", "01224000000YAuK"),
+            relationship(
+              "Person__r",
+              "CommCare_ID__c",
+              dataValue("form.case.@case_id")
+            ),
+          )
+        )
+      ))(state);
+    }
+    console.log("No ART Treatment referral.");
+    return state;  
+});
 
 
