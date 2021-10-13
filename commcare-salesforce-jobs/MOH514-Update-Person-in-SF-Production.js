e30cb5eb890f286cfae4b62064e26742f47100a0
Taylor Downs <taylor@openfn.org>
2021-10-13 15:03:58 +0100

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
+++ commcare-salesforce-jobs/MOH514-Update-Person-in-SF-Production.js
@@ -3093,3 +3093,42 @@ alterState((state) => {
     console.log("No Pregnancy referral.");
     return state;  
 });
+//Family Plannning
+alterState((state) => {
+  if (
+    dataValue("form.TT5/Mother_Information/referred_for_family_planning_services")(state) == "yes"
+  ) {
+    return upsert(
+      "Service__c",
+      "Service_UID__c",
+      fields(
+        field("CommCare_Code__c", (state) => {
+          var id = dataValue("id")(state);
+          var serviceId = id + "family_planning";
+          return serviceId;
+        }),
+        field("Service_UID__c", (state) => {
+          var id = dataValue("id")(state);
+          var serviceId = id + "family_planning";
+          return serviceId;
+        }),
+        field("Household_CHW__c", dataValue("form.CHW_ID_Final"),
+          field("Type_of_Service__c", "Family Planning"),
+          field("Reason_for_Service__c", "Family Planning (FP)"),
+          field("Date__c", dataValue("form.case.update.Date"),
+            field("RecordTypeID", "01224000000YAuK"),
+            relationship(
+              "Person__r",
+              "CommCare_ID__c",
+              dataValue("form.case.@case_id")
+            ),
+          )
+        )
+      ))(state);
+    }
+    console.log("No Family Planning referral.");
+    return state;  
+});
+
+
+
