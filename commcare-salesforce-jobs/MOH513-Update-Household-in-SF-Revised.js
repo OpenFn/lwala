e5f50a7149ca4f2900940d4965559512e4edc4c2
Taylor Downs <taylor@openfn.org>
2021-11-22 10:13:54 +0000

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- commcare-salesforce-jobs/MOH513-Update-Household-in-SF-Revised.js
+++ commcare-salesforce-jobs/MOH513-Update-Household-in-SF-Revised.js
@@ -109,14 +109,6 @@ upsert(
     field("Other_Health_Insurance__c", dataValue("form.if_other_please_specify"))
   )
 ),
-
-//New Nutrition Field (MOTG)
- field("Active_in_lwala_nutrition_program__c",dataValue("form.case.update.enrolled_in_a_lwala_nutrition_program")),
- field("lwala_nutrition_program_enrollment_date__c",dataValue("form.case.update.lwala_nutrition_program_enrollment_date")),
- field("Trained_in_gardening__c",dataValue('form.household_trained_on_gardening')),
- field("household_trained_on_gardening_date__c",dataValue('form.case.update.when_was_the_household_trained_on_gardening')),
- field("provided_with_seed_input_support__c",dataValue('form.case.update.household_provided_with_seed_input_support')),
- field("household_provided_with_seed_input_suppo__c",dataValue('form.case.update.when_was_the_household_provided_with_seed_input_support')),
   upsert(
     'Visit__c',
     'CommCare_Visit_ID__c',
