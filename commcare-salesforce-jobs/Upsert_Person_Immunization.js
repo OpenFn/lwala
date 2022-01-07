b540ec64225e5119532d1c395d483a825f24953b
Aleksa Krolls <aleksagk@gmail.com>
2022-01-07 00:06:08 -0700

    aleksa@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/Upsert_Person_Immunization.js
+++ commcare-salesforce-jobs/Upsert_Person_Immunization.js
@@ -4,13 +4,13 @@ upsert(
   fields(
     field("CommCare_ID__c", dataValue("case_id")),
     relationship("Household__r","CommCare_Code__c", dataValue("indices.parent.case_id")),
-    field("BCG__c", dataValue("properties.CHW.Follow-Up.Immunizations.BCG")),
-    field("OPV_0__c", dataValue("properties.CHW.Follow-Up.Immunizations.OPV_0")),
-    field("OPV_1__c", dataValue("properties.CHW.Follow-Up.Immunizations.OPV_PCV_Penta_1")),
-    field("OPV_2__c", dataValue("properties.CHW.Follow-Up.Immunizations.OPV_PCV_Penta_2")),
-    field("OPV_3__c", dataValue("properties.CHW.Follow-Up.Immunizations.OPV_PCV_Penta_3")),
-    field("Measles_6__c", dataValue("properties.CHW.Follow-Up.Immunizations.Measles_6")),
-    field("Measles_9__c", dataValue("properties.CHW.Follow-Up.Immunizations.Measles_9")),
-    field("Measles_18__c", dataValue("properties.CHW.Follow-Up.Immunizations.Measles_18"))
+    field("BCG__c", dataValue("properties.BCG")),
+    field("OPV_0__c", dataValue("properties.OPV_0")),
+    field("OPV_1__c", dataValue("properties.OPV_PCV_Penta_1")),
+    field("OPV_2__c", dataValue("properties.OPV_PCV_Penta_2")),
+    field("OPV_3__c", dataValue("properties.OPV_PCV_Penta_3")),
+    field("Measles_6__c", dataValue("properties.Measles_6")),
+    field("Measles_9__c", dataValue("properties.Measles_9")),
+    field("Measles_18__c", dataValue("properties.Measles_18"))
   )
 ); 
