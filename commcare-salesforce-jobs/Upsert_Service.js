0a0b1cc7b308bbd4ef9a3c526740e00700ed9a1d
Elias W. BA <elias@openfn.org>
2022-01-07 12:36:57 +0000

    WIP

--- commcare-salesforce-jobs/Upsert_Service.js
+++ commcare-salesforce-jobs/Upsert_Service.js
@@ -13,126 +13,111 @@ fn((state) => {
     Other: "Other",
   };
 
-  return { ...state, facilityMap };
-});
+  let serviceMapping = {};
 
-upsert(
-  "Service__c",
-  "Service_UID__c",
-  fields(
-    field("Service_UID__c", dataValue("case_id")),
-    field("CommCare_Code__c", dataValue("case_id")),
-    field("RecordTypeID", "01224000000YAuK"),
-    field("Household_CHW__c", "a030Q000008XyXV"), //Sandbox test CHW
-    // relationship( //ADD BACK BEFORE PROD DEPLOYMENT; removed for sandbox testing
-    //   "Household_CHW__r",
-    //   "CommCare_ID__c",
-    //   dataValue("properties.CHW_ID")
-    // ),
-    //TODO: Only map this if state.data.indices.parent.case_type = "Person"
+  // {
+  //   Service_UID__c: state.data.case_id,
+  //   CommCare_Code__c: state.data.case_id,
+  //   RecordTypeID: "01224000000YAuK",
+  //   Household_CHW__c: "a030Q000008XyXV",
+  //   Open_Case__c: state.data.closed,
+  //   Source__c: state.data.properties.Source === "1",
+  //   Clinical_facility__c: (facilityMap) => {
+  //     const facility =
+  //       state.data.properties.referred_facility ||
+  //       state.data.properties.CHW.Facility_Services.Facility;
+  //     return facilityMap[facility];
+  //   },
+  //   Client_Received_Services_at_Facility__c:
+  //     state.data.properties.CHW.Facility_Services.Facility_Visit,
+  //   Clinical_Visit_Date__c:
+  //     state.data.properties.CHW.Facility_Services.Facility_Date,
+  //   CHW_Followed_Up_with_the_Client__c:
+  //     state.data.properties.CHW.Follow - Up.Follow - Up,
+  //   Follow_Up_Date__c: state.data.properties.CHW.Follow - Up.Follow - Up_Date,
+  //   Person_Complied_w_Referral_in_24_hrs__c:
+  //     state.data.properties.CHW["Follow-Up"].referral_compliance,
+  //   Skillled_Delivery__c:
+  //     state.data.properties.CHW["Follow-Up"].skilled_delivery,
+  //   Child_received_immunizations:
+  //     state.data.properties.CHW["Follow-Up"].immunization,
+  //   Received_a_diagnosis_for_PSBI__c:
+  //     state.data.properties.CHW["Follow-Up"].PSBI.psbi_diagnosis,
+  //   Received_antibiotics_per_protocol__c:
+  //     state.data.properties.CHW["Follow-Up"].PSBI.antibiotic_8days,
+  //   Distributed_Treatment_on_Last_Visit__c:
+  //     state.data.properties.CHW["Follow-Up"].distribute_treatment,
+  //   Person_had_an_adverse_drug_reaction__c:
+  //     state.data.properties.CHW["Follow-Up"].adverse_drug_reaction,
+  //   Defaulted__c:
+  //     state.data.properties.CHW["Follow-Up"].default_on_treatment === "yes",
+  //   Date_of_Default__c: state.data.properties.CHW["Follow-Up"].date_of_default,
+  //   Client_s_Symptoms_Improved__c:
+  //     state.data.properties.CHW["Follow-Up"].Client_Improved,
+  //   Case_Type__c: state.data.properties.Case_Type,
+  //   Follow_Up_By_Date__c: (state) => {
+  //     var date = state.data.properties["Follow-Up_By_Date"];
+  //     return date && date !== "" ? date : undefined;
+  //   },
+  //   Date__c: new Date(state.data.properties.date_opened).toISOString(),
+  //   Reason_for_Service__c: state.data.properties.Reason_for_Service,
+  //   Type_of_Service__c: state.data.properties.Type_of_Service,
+  //   Malaria_Status__c: state.data.properties.Malaria_Status,
+  //   Home_Treatment_Date__c: state.data.properties.home_treatment_date,
+  //   Malaria_Home_Test_Date__c: state.data.properties.malaria_test_date,
+  //   Home_Based_Care_Rendered__c: (state) => {
+  //     const hbc = state.data.properties.Home_Based_Care_Provided;
+  //     return hbc && hbc.split(" ").join(";");
+  //   },
+  //   Home_ORS__c: state.data.properties.clinic_ors,
+  //   Home_Zinc__c: state.data.properties.clinic_zinc,
+  //   Height__c: state.data.properties.height,
+  //   Weight__c: state.data.properties.weight,
+  //   MUAC__c: state.data.properties.muac,
+  //   Nutrition_Status__c: state.data.properties.Nutrition_Status,
+  // };
+
+  const personRelationship = (state) =>
     relationship(
       "Person__r",
       "CommCare_ID__c",
-      dataValue("indices.parent.case_id")
+      state.data.indices.parent.case_id
+    );
+
+  const caseRelationship = (state) => [
+    relationship(
+      "Parent_Service__r",
+      "Service_UID__c",
+      state.data.indices.parent.case_id
     ),
-    //TODO: Only map this if state.data.indices.parent.case_type = "Case"
-    // relationship(
-    //   "Parent_Service__r",
-    //   "Service_UID__c",
-    //   dataValue("indices.parent.case_id")
-    // ),
-    field("Open_Case__c", dataValue("closed")),
-    field("Source__c", dataValue("properties.Source") === "1"),
-    field("Clinical_facility__c", (state) => {
-      var chwf = dataValue("properties.CHW.Facility_Services.Facility")(state);
-      var fac = dataValue("properties.referred_facility")(state);
-      var facility = fac || chwf;
-      return state.facilityMap[facility];
-    }),
-    field(
-      "Client_Received_Services_at_Facility__c",
-      dataValue("properties.CHW.Facility_Services.Facility_Visit")
+    relationship(
+      "Person__r",
+      "CommCare_ID__c",
+      query(
+        `SELECT Person__r.CommCare_ID__c FROM Service__c WHERE Service_UID__c = ${state.data.indices.parent.case_id}`
+      )(state)
     ),
-    field(
-      "Clinical_Visit_Date__c",
-      dataValue("properties.CHW.Facility_Services.Facility_Date")
-    ),
-    field(
-      "CHW_Followed_Up_with_the_Client__c",
-      dataValue("properties.CHW.Follow-Up.Follow-Up")
-    ),
-    field(
-      "Follow_Up_Date__c",
-      dataValue("properties.CHW.Follow-Up.Follow-Up_Date")
-    ),
-    field(
-      "Person_Complied_w_Referral_in_24_hrs__c",
-      dataValue("properties.CHW.Follow-Up.referral_compliance")
-    ),
-    field(
-      "Skillled_Delivery__c",
-      dataValue("properties.CHW.Follow-Up.skilled_delivery")
-    ),
-    field(
-      "Child_received_immunizations",
-      dataValue("properties.CHW.Follow-Up.immunization")
-    ),
-    field(
-      "Received_a_diagnosis_for_PSBI__c",
-      dataValue("properties.CHW.Follow-Up.PSBI.psbi_diagnosis")
-    ),
-    field(
-      "Received_antibiotics_per_protocol__c",
-      dataValue("properties.CHW.Follow-Up.PSBI.antibiotic_8days")
-    ),
-    field(
-      "Distributed_Treatment_on_Last_Visit__c",
-      dataValue("properties.CHW.Follow-Up.distribute_treatment")
-    ),
-    field(
-      "Person_had_an_adverse_drug_reaction__c",
-      dataValue("properties.CHW.Follow-Up.adverse_drug_reaction")
-    ),
-    field(
-      "Defaulted__c",
-      dataValue("properties.CHW.Follow-Up.default_on_treatment") === "yes"
-    ),
-    field(
-      "Date_of_Default__c",
-      dataValue("properties.CHW.Follow-Up.date_of_default")
-    ),
-    field(
-      "Client_s_Symptoms_Improved__c",
-      dataValue("properties.CHW.Follow-Up.Client_Improved")
-    ),
-    field("Case_Type__c", dataValue("properties.Case_Type")),
-    field("Follow_Up_By_Date__c", (state) => {
-      var date = dataValue("properties.Follow-Up_By_Date")(state);
-      return date && date !== "" ? date : undefined;
-    }),
-    field("Date__c", (state) =>
-      new Date(state.data.properties.date_opened).toISOString()
-    ),
-    field("Reason_for_Service__c", dataValue("properties.Reason_for_Service")),
-    field("Type_of_Service__c", dataValue("properties.Type_of_Service")),
-    field("Malaria_Status__c", dataValue("properties.Malaria_Status")),
-    field(
-      "Home_Treatment_Date__c",
-      dataValue("properties.home_treatment_date")
-    ),
-    field(
-      "Malaria_Home_Test_Date__c",
-      dataValue("properties.malaria_test_date")
-    ),
-    field("Home_Based_Care_Rendered__c", (state) => {
-      const hbc = state.data.properties.Home_Based_Care_Provided;
-      return hbc && hbc.split(" ").join(";");
-    }),
-    field("Home_ORS__c", dataValue("properties.clinic_ors")),
-    field("Home_Zinc__c", dataValue("properties.clinic_zinc")),
-    field("Height__c", dataValue("properties.height")),
-    field("Weight__c", dataValue("properties.weight")),
-    field("MUAC__c", dataValue("properties.muac")),
-    field("Nutrition_Status__c", dataValue("properties.Nutrition_Status"))
-  )
-);
+  ];
+
+  if (state.data.indices.parent.case_type === "Person") {
+    const Person__r = personRelationship(state);
+    serviceMapping = { ...serviceMapping, [Person__r[0]]: Person__r[1] };
+  }
+
+  if (state.data.indices.parent.case_type === "Case") {
+    const casesRelationships = caseRelationship(state);
+    const Parent_Service__r = casesRelationships[0];
+    const Person__r = casesRelationships[0];
+    serviceMapping = {
+      ...serviceMapping,
+      [Parent_Service__r[0]]: Parent_Service__r[1],
+      [Person__r[0]]: Person__r[1],
+    };
+  }
+
+  console.log("SERVICE MAPPING", serviceMapping);
+
+  return { ...state, serviceMapping };
+});
+
+// upsert("Service__c", "Service_UID__c", (state) => state.serviceMapping);
