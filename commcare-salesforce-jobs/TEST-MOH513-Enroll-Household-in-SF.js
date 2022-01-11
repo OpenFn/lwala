a86132308d185b707fad7de8ec6c00ce24e462e4
Aleksa Krolls <aleksagk@gmail.com>
2022-01-11 14:34:28 +0200

    aleksa@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/TEST-MOH513-Enroll-Household-in-SF.js
+++ commcare-salesforce-jobs/TEST-MOH513-Enroll-Household-in-SF.js
@@ -45,10 +45,11 @@ upsert(
     field("MOH_household_code__c", dataValue("form.moh_code")),
     field("CommCare_Code__c", dataValue("form.case.@case_id")),
     field("Source__c", true),
-    field("Household_CHW__c", state => {
-      var chw = dataValue("form.CHW_ID")(state);
-      return chw === 'a030800001zQrk' ? 'a030800001zQrk5' : chw ? chw : undefined;
-    }),
+    field("Household_CHW__c", "a030Q000008XyXV"), //for sandbox testing
+    // field("Household_CHW__c", state => {
+    //   var chw = dataValue("form.CHW_ID")(state);
+    //   return chw === 'a030800001zQrk' ? 'a030800001zQrk5' : chw ? chw : undefined;
+    // }),
     //field("Household_CHW__c", "a031x000002S9lm"), //HARDCODED FOR SANDBOX TESTING --> To replace with line above
     //== LOCATION FIELD UPDATESS =====================//
     relationship("Catchment__r", "Name", state => {
