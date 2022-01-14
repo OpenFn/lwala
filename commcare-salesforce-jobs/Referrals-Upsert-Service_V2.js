ff41076d3f04abc5cbb79f49073e5c90da5bb6e1
aleksa-krolls <aleksagk@gmail.com>
2022-01-14 14:25:28 +0200

    new draft mappings for multiselect fields

--- commcare-salesforce-jobs/Referrals-Upsert-Service_V2.js
+++ commcare-salesforce-jobs/Referrals-Upsert-Service_V2.js
@@ -31,6 +31,116 @@ fn(state => {
     Other: 'Other',
   };
 
+  const pregDangerMap = {
+    Vaginal_Bleeding: 'Vaginal Bleeding',
+    Water_Breaks: 'Water Breaks before Time of Delivery',
+    Prolonged_Labour: 'Prolonged Labour over 12 Hours',
+    Convulsions: 'Convulsions or Fits',
+    Abdominal_Pain: 'Severe Abdominal Pain before Delivery',
+    High_Fever: 'High Fever',
+    Low_Baby_Movement: 'Feeling the Baby move less or not at all',
+    Swelling: 'Swelling of Face and Hands',
+    Severe_Headache: 'Severe or Continuous Headache for more than 12 hours',
+    Severe_Vomiting: 'Severe or Continuous Vomiting',
+    none: 'None',
+  };
+
+  const serviceMap = {
+    Scheduled_PSC_Apt: 'Scheduled PSC Apt',
+    Adverse_Drug_Reaction_Side_Effect: 'Adverse Drug Reaction/Side Effect',
+    Malnutrition: 'Malnutrition',
+    Malaria: 'Malaria',
+    TB: 'TB',
+    Treatment_for_Other_OIs: 'Treatment for other Ols',
+    ARI: 'ARI',
+    Anemia: 'Anemia',
+    Diarrhea: 'Diarrhea',
+    Pregnancy_Care: 'Pregnancy Care (ANC)',
+    Family_Planning: 'Family Planning (FP)',
+    Preconception_Counseling: 'Preconception Counseling',
+    Injury: 'Injury',
+    Other: 'Other',
+  };
+
+  const milestoneTypeMap = {
+    cognitive_delays_learning_difficulties:
+      'Cognitive Delays Learning Difficulties',
+    motor_delays: 'Motor Delays',
+    speech_and_language_delay: 'Delay Speech and Language Delay',
+    social_and_emotional: 'Social and emotional',
+  };
+
+  const symptomsMap = {
+    convulsions: 'Convulsions',
+    not_able_to_eat_drink: 'Not able to eat/drink',
+    vomits_everything: 'Vomits everything',
+    chest_in_drawing: 'Chest in-drawing',
+    unusally_sleepy_unconscious: 'Unusually sleepy/unconscious',
+    swelling_of_both_feet: 'Swelling of both feet',
+  };
+
+  const childSignMap = {
+    comm: 'Poor Breastfeeding (under 6 months old child)',
+    comm: 'Not able to feed since birth, or stopped feeding well',
+    comm: 'Not able to breastfeed',
+    comm: 'Fever (37.5 C or more)',
+    comm: 'Very low temperature (35.4 C or less)',
+    comm: 'Shivering',
+    comm: 'Fast Breathing',
+    comm: 'Very Sleepy',
+    comm: 'Convulsions and Fits',
+    comm: 'Only moves when stimulated, or does not move even on stimulation',
+    comm: 'Yellow sole(Baby body turning yellow especially eyes, palms, soles)',
+    comm: 'Bleeding from the umbilical stump',
+    comm: 'Signs of local infection: umbilicus is red or draining pus, skin boils, or eyes draining pus',
+    comm: 'Weight chart using color coded scales if RED or YELLOW(Weight below 2.5kg or born less than 36 weeks of age)',
+    comm: 'Unable to cry',
+    comm: 'Cyanosis',
+    comm: 'Bulging fontanelle',
+  };
+
+  const otherReferralMap = {
+    comm: 'HIV Testing and Counseling',
+    comm: 'Visit to Clinician',
+    comm: 'Adverse Drug Reaction Side Effect',
+    comm: 'Malnutrition',
+    comm: 'Malaria',
+    comm: 'PMTCT',
+    comm: 'TB',
+    comm: 'Treatment for other OIs',
+    comm: 'ARI',
+    comm: 'Anemia',
+    comm: 'Diarrhea',
+    comm: 'Pregnancy Care (ANC)',
+    comm: 'Family Planning (FP)',
+    comm: 'Preconception Counseling',
+    comm: 'Injury',
+    comm: 'Blood in Stool',
+    comm: 'Immunization',
+    comm: 'Routine Health Check ups',
+    comm: 'Other',
+  };
+
+  const homeCareMap = {
+    Adherence_Counseling: 'Adherence Counseling',
+    Pill_Count_Monitoring: 'Pill Count Monitoring',
+    Nutrition_Assessment_and_Counseling: 'Nutrition Assessment and Counseling',
+    WASH_Counseling: 'WASH Counseling',
+    Prevention_Counseling: 'Prevention Counseling',
+    Psychosocial_Support: 'Psychosocial Support',
+    Provision_of_Supplies: 'Provision of Supplies',
+    OI_Management_Support: 'OI Management Support',
+  };
+
+  const clinicalMap = {
+    comm: 'Diarrhea',
+    comm: 'Malnutrition',
+    comm: 'Malaria',
+    comm: 'Acute Respiratoy Infection (ARI)',
+    comm: 'Accident/Injury',
+    comm: 'Other',
+  };
+
   let relationships = [];
 
   // If it's a person, add the person relationship
@@ -56,7 +166,20 @@ fn(state => {
     relationships.push(relationship('Person__r', 'CommCare_ID__c', state.ccId));
   }
 
-  return { ...state, facilityMap, relationships };
+  return {
+    ...state,
+    facilityMap,
+    relationships,
+    serviceMap,
+    pregDangerMap,
+    milestoneTypeMap,
+    symptomsMap,
+    childSignMap,
+    otherReferralMap,
+    homeCareMap,
+    clinicalMap,
+    psbiMap,
+  };
 });
 
 // NOTE: We finally upsert to the Service__c object in Salesforce
@@ -84,10 +207,9 @@ upsert('Service__c', 'Service_UID__c', state => ({
       var facility = fac || chwf;
       return state.facilityMap[facility];
     }),
-    field(
-      'Client_Received_Services_at_Facility__c', state => {
+    field('Client_Received_Services_at_Facility__c', state => {
       var serv = dataValue('properties.Facility_Visit')(state);
-      return serv==='Yes' || serv==='yes' ? true : false; 
+      return serv === 'Yes' || serv === 'yes' ? true : false;
     }),
     field('Clinical_Visit_Date__c', state => {
       var date = dataValue('properties.Facility_Date')(state);
@@ -151,15 +273,60 @@ upsert('Service__c', 'Service_UID__c', state => ({
       'Malaria_Home_Test_Date__c',
       dataValue('properties.malaria_test_date')
     ),
-    field('Home_Based_Care_Rendered__c', state => {
-      const hbc = state.data.properties.Home_Based_Care_Provided;
-      return hbc && hbc.split(' ').join(';');
-    }),
     field('Home_ORS__c', dataValue('properties.clinic_ors')),
     field('Home_Zinc__c', dataValue('properties.clinic_zinc')),
     field('Height__c', dataValue('properties.height')),
     field('Weight__c', dataValue('properties.weight')),
     field('MUAC__c', dataValue('properties.muac')),
-    field('Nutrition_Status__c', dataValue('properties.Nutrition_Status'))
+    field('Nutrition_Status__c', dataValue('properties.Nutrition_Status')),
+    //===== NEW MAPPINGS - JAN 14 ===========================//
+    field('Pregnancy_Danger_Signs__c', state => {
+      var sign = dataValue('properties.pregnancy_danger_signs')(state);
+      return sign && sign.split(' ').join(';');
+      //TODO:apply state.pregDangerMap to each 'sign' value which is multiselect in commcare
+    }),
+    field('Child_Danger_Signs__c', state => {
+      var check = dataValue('properties.Other_Danger_Signs')(state);
+      return check && check.split(' ').join(';');
+      //TODO:apply state.childSignMap to each 'check' value which is multiselect in commcare
+    }),
+    field('Delayed_Milestone__c', state => {
+      var check = dataValue('properties.which_delayed_milestone')(state);
+      return check && check.split(' ').join(';');
+      //TODO: apply state.milestoneTypeMap to each 'check' value which is multiselect in commcare
+    }),
+    field('Serious_Symptoms__c', state => {
+      var check = dataValue('properties.symptoms_check_other')(state);
+      return check && check.split(' ').join(';');
+      //TODO: apply state.symptomsMap to each 'check' value which is multiselect in commcare
+    }),
+    field('Other_Referral_Reasons__c', state => {
+      var check = dataValue('properties.Purpose_of_Referral')(state);
+      return check && check.split(' ').join(';');
+      //TODO: apply state.otherReferralMap to each 'check' value which is multiselect in commcare
+    }),
+
+    field('Home_Based_Care_Rendered__c', state => {
+      const hbc = dataValue('properties.Home_Based_Care_Provided')(state);
+      return hbc ? hbc.split(' ').join(';').replace(/_/g, ' ') : hbc;
+      //TODO: apply state.homeCareMap to each 'hbc' value which is multiselect in commcare
+    }),
+    field('PSBI_Visit__c', state => {
+      var number = dataValue('properties.psbi_task')(state);
+      return `Day ${number}`; //sample output: 'Day 3'
+    }),
+    field('Clinical_Services__c', state => {
+      var check = dataValue('properties.TT5_Clinical_Service')(state);
+      return state.clinicalMap[check];
+    }),
+    field('Referred_Facility__c', state => {
+      var check = dataValue('properties.referred_facility')(state);
+      return state.facilityMap[check];
+    }),
+    field('HAWI_Clinical_Services__c', state => {
+      var sign = dataValue('properties.HAWI_Clinical_Service')(state);
+      return state.serviceMap[sign];
+    })
+    //=====================================//
   ),
 }));
