e46eccde4367590cafa895b20daf19b04da669c8
Taylor Downs <taylor@openfn.org>
2022-02-25 07:59:43 +0000

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- commcare-salesforce-jobs/Case-Upsert-Household.js
+++ commcare-salesforce-jobs/Case-Upsert-Household.js
@@ -1,31 +1,38 @@
-titleCase = str => {
-    var splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
-      splitStr[i] =
-        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    return splitStr.join(' ');
-  };
+//fn(state => {
+//  const person = state.data.form.Person;
+//  if (!Array.isArray(person)) {
+//    state.data.form.Person = [person];
+//  }
 
-  const reasonMapping = {
-    pregnant: 'The client is pregnant',
-    intentions_of_getting_pregnant: 'Intentions of getting pregnant',
-    lack_of_access_to_fp_information: 'Lack of access to FP information',
-    not_sexually_active: 'The client is not sexually active',
-    other_barriers_culture_male_partners_parents_etc:
-      'Other barriers (culture, male partners, parents, etc)',
-    no_access_to_fp_services_hospitals:
-      'Lack of hospitals or places where FP services can be accessed',
-    not_willing_to_use_fp_due_to_negative_effects_myths_and_misconceptions:
-      'Myths and misconceptions',
-    barriers_at_service_delivery_points: 'Barriers at service delivery points',
-  };
+//  titleCase = str => {
+//    var splitStr = str.toLowerCase().split(' ');
+//    for (var i = 0; i < splitStr.length; i++) {
+//      splitStr[i] =
+//        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+//    }
+//    return splitStr.join(' ');
+//  };
 
-  state.area = state.data.properties.area_name; 
-  state.catchment = state.data.properties.catchment_name;
+//Not necessary - relates to a person (MOTG)
+ // const reasonMapping = {
+ //   pregnant: 'The client is pregnant',
+ //   intentions_of_getting_pregnant: 'Intentions of getting pregnant',
+ //   lack_of_access_to_fp_information: 'Lack of access to FP information',
+ //   not_sexually_active: 'The client is not sexually active',
+ //   other_barriers_culture_male_partners_parents_etc:
+ //     'Other barriers (culture, male partners, parents, etc)',
+ //   no_access_to_fp_services_hospitals:
+ //     'Lack of hospitals or places where FP services can be accessed',
+ //   not_willing_to_use_fp_due_to_negative_effects_myths_and_misconceptions:
+ //     'Myths and misconceptions',
+ //   barriers_at_service_delivery_points: 'Barriers at service delivery points',
+ // };
 
-  return { ...state, reasonMapping };
-});
+//  state.area = state.data.properties.area_name; 
+//  state.catchment = state.data.properties.catchment_name;
+
+//  return { ...state, reasonMapping };
+//});
 
 //Upserting Household, checks if Household exists via MOH Household Code
 upsert(
