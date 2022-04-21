5e0a832e16952317f487285907925467bfa24fb8
Taylor Downs <taylor@openfn.org>
2022-04-21 12:54:53 +0100

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- commcare-salesforce-jobs/Case-Upsert-Household.js
+++ commcare-salesforce-jobs/Case-Upsert-Household.js
@@ -82,6 +82,7 @@ upsert(
     field('Cookstove__c',dataValue('properties.Improved_Cooking_Method')),//not coming through
     field('Clothe__c',dataValue('properties.Clothesline')),//not coming through
     field('WASH_Trained__c',dataValue('properties.WASH_Trained')),//not coming through
+    field('Uses_ITNs__c',dataValue('properties.ITNs')),
     field('Total_household_people__c',dataValue('properties.Total_Number_of_Members')), //not coming through
     field('Health_insurance__c', dataValue('properties.health_insurace_cover')),
     field('Health_insurance_active_status__c',dataValue('properties.healthinsurance_active')),
@@ -99,6 +100,61 @@ upsert(
     field('Work_with_TBA__c', dataValue('properties.tba')),
     field('TBA_name__c', dataValue('properties.which_tba')),
     field('Last_Modified_Date_CommCare__c', dataValue('server_date_modified')),//Need a case property),
+    field('Active_Household__c', state => {
+      var status = dataValue('properties.Household_Status')(state);
+      return status && status === 'No'
+        ? false
+        : status === 'Yes'
+        ? true
+        : status;
+    }),
+     field('Inactive_Reason__c', state => {
+      var reason = dataValue('properties.Reason_for_Inactive')(state);
+      return reason ? reason.toString().replace(/_/g, ' ') : null;
+    }),
+     field(
+      'Active_in_Nutrition_Program__c',
+      dataValue(
+        'form.nutrition_enrollment.enrolled_in_a_lwala_nutrition_program'
+      )
+    ),
+    field(
+      'lwala_nutrition_program_enrollment_date__c',
+      dataValue(
+        'properties.nutrition_enrollment.lwala_nutrition_program_enrollment_date'
+      )
+    ),
+    field(
+      'Trained_in_gardening__c',
+      dataValue('properties.nutrition_enrollment.household_trained_on_gardening')
+    ),
+    field(
+      'household_trained_on_gardening_date__c',
+      dataValue(
+        'properties.nutrition_enrollment.when_was_the_household_trained_on_gardening'
+      )
+    ),
+    field(
+      'Seed_Input_Support__c',
+      dataValue(
+        'properties.nutrition_enrollment.household_provided_with_seed_input_support'
+      )
+    ),
+    field(
+      'household_provided_with_seed_input_suppo__c',
+      dataValue(
+        'properties.nutrition_enrollment.when_was_the_household_provided_with_seed_input_support'
+      )
+    ),
+    field(
+      'MIYCN_Trained__c',
+      dataValue('properties.nutrition_enrollment.household_trained_on_MIYCN')
+    ),
+    field(
+      'Kitchen_Garden__c',
+      dataValue('properties.nutrition_enrollment.household_has_kitchen_garden')
+    ),
+
     //field('Case_Closed_Date__c', state => {
     //  var closed = dataValue('date_closed')(state); 
     //  var date =  dataValue('server_date_modified')(state); 
