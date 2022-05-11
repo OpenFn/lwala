465fd5f0d7d924aaef2c0a76059672f7e4cfdc4f
Rita Zágoni <zagoni.rita@gmail.com>
2022-05-11 10:26:28 +0300

    rita@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/Case-Upsert-Household.js
+++ commcare-salesforce-jobs/Case-Upsert-Household.js
@@ -54,10 +54,10 @@ upsert(
   //   }), // check
   //   field('Area__c', state => {
   //    // var area = dataValue('properties.Area_Name')(state);
-  //      var area = dataValue('properties.area')(state);
   //     return area === '' || area === undefined ? 'a000Q00000Egmu4' : area;
   //   }),  // Commented out because it was causing a job error 
   //   field('Household_village__c', dataValue('properties.village')),//case property, but not in message
+  
   //   field('Village__c',dataValue('properties.village_name')), //lookup
     field('Deaths_in_the_last_6_months__c', state => {
       var death = dataValue(
