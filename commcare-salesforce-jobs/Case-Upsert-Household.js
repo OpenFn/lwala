3b91457db939cfe223ab196ef8300e3fb91ff129
Rita Zágoni <zagoni.rita@gmail.com>
2022-05-19 16:12:46 +0300

    rita@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/Case-Upsert-Household.js
+++ commcare-salesforce-jobs/Case-Upsert-Household.js
@@ -31,9 +31,9 @@ upsert(
   fields(
     field('CommCare_Username__c', dataValue('form.meta.username')),//Need a case property
     field('MOH_household_code__c', dataValue('properties.moh_code')),
-    field('CommCare_Code__c', dataValue('case_id') || dataValue('form.case.@case_id')),
+   // field('CommCare_Code__c', dataValue('case_id')),
     fied('CommCare_Code__c', state => {
-    //  var cc_code = dataValue('case_id')(state)
+      var cc_code = dataValue('case_id')(state)
       return cc_code === undefined ? dataValue('form.case.@case_id') : cc_code;
       }),
     field('Source__c', true),
