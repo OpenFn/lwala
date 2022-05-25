463ebced41a3c230e6bad1671517f1278175cee6
Rita Zágoni <zagoni.rita@gmail.com>
2022-05-25 13:16:02 +0300

    rita@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/test_jobs/TEST-Upsert-Person-Update-Form.js
+++ commcare-salesforce-jobs/test_jobs/TEST-Upsert-Person-Update-Form.js
@@ -133,7 +133,9 @@ upsert(
           dataValue('form.TT5.Child_Information.Deworming')
         ),
         field('Source__c', 1),*/
-    field('CommCare_ID__c', dataValue('form.case.@case_id')),
+    /field('CommCare_ID__c', dataValue('form.case.@case_id')),
+    field('CommCare_ID__c', dataValue('id')),
+
     relationship(
       'Person__r',
       'CommCare_Code__c',
