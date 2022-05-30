179665ba2aef572b047a2b8ca715b38123e0ece4
Taylor Downs <taylor@openfn.org>
2022-05-30 19:14:45 +0100

    second concept

--- bulk/bulk-fetch.js
+++ bulk/bulk-fetch.js
@@ -1,44 +1,43 @@
 fn(state => {
+  const baseUrl =
+    'https://www.commcarehq.org/a/lwala-community-alliance/api/v0.5/form/';
+
   const formIds = [
     '457C806C-B47D-44F0-BE4B-7E88F7162D1D',
     '320142AD-BC92-4470-951E-B3CA140BDC4A',
     '318B2FE0-F17F-4FC2-8EBE-1FF170F25B3F',
   ];
-  return { ...state, formIds, payloads: [] };
+
+  const queries = formIds.map(
+    id =>
+      `?xmlns=http://openrosa.org/formdesigner/${id}` +
+      `&received_on_end=2019-12-31` +
+      `&limit=50`
+  );
+
+  return { ...state, queries, baseUrl, payloads: [] };
 });
 
 each(
-  '$.formIds[*]',
-  fn(state => {
-    console.log('Requesting data for form:', state.data);
-    return get(
-      'https://www.commcarehq.org/a/lwala-community-alliance/api/v0.5/form/',
-      {
-        query: {
-          limit: 2, // max limit
-          offset:
-            state.meta && state.meta.next
-              ? state.meta.limit + state.meta.offset
-              : 0,
-          xmlns: `http://openrosa.org/formdesigner/${state.data}`,
-          received_on_end: '2019-12-31',
-        },
-      },
-      nextState => {
-        console.log('Metadata in CommCare response:');
-        console.log(nextState.data.meta);
+  '$.queries[*]',
+  get(
+    state => `${state.baseUrl}${state.data}`,
+    {},
+    nextState => {
+      const { baseUrl, queries, data, payloads } = nextState;
+      const { meta, objects } = data;
+      console.log('Metadata in CommCare response:', meta);
 
-        return {
-          ...nextState,
-          payloads: [...nextState.payloads, ...nextState.data.objects],
-        };
-      }
-    )(state);
-  })
+      if (meta.next) queries.push(`${baseUrl}${meta.next}`);
+
+      return { ...nextState, payloads: [...payloads, ...objects] };
+    }
+  )
 );
 
 fn(state => {
-  console.log(state.payloads.length);
+  console.log('Count of payloads', state.payloads.length);
+  console.log('Count of queries', state.queries.length);
   return state;
 });
 
