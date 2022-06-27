40446ab90e192070a7bb78cc41af643f7a5761f9
Taylor Downs <taylor@openfn.org>
2022-06-27 18:55:33 +0200

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- commcare-salesforce-jobs/MOH514-Update-Person_V2.js
+++ commcare-salesforce-jobs/MOH514-Update-Person_V2.js
@@ -319,7 +319,7 @@ fn(state => {
           dataValue('form.ANCs.pregnancy_danger_signs.Delivery_Information.DOB')
         ),
         field('Place_of_Delivery__c', state => {
-          var facility = dataValue('properties.Delivery_Type')(
+          var facility = dataValue('form.TT5.Child_Information.Delivery_Type')(
             state
           );
           return facility === 'Skilled'
