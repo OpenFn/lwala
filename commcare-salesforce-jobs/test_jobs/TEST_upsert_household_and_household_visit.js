6b409784775b9ace7d79d71174e11e56c949b429
Rita Zágoni <zagoni.rita@gmail.com>
2022-07-01 08:07:58 +0300

    rita@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/test_jobs/TEST_upsert_household_and_household_visit.js
+++ commcare-salesforce-jobs/test_jobs/TEST_upsert_household_and_household_visit.js
@@ -377,12 +377,12 @@ upsertIf(
     ),
     //field('Last_Modified_Date_CommCare__c', dataValue('server_modified_on')),
     //field('CommCare_Form_Opened__c',dataValue('properties.last_form_opened_date_and_time')),
-    field('CommCare_Form_Opened__c', state=> {
-      var form_opened = dataValue('properties.last_form_opened_date_and_time')(state);
-      var date  = new Date(form_opened)(state);
-      var iso = date.toISOString()(state);
-      return iso;
-    }),
+    // field('CommCare_Form_Opened__c', state=> {
+    //   var form_opened = dataValue('properties.last_form_opened_date_and_time')(state);
+    //   var date  = new Date(form_opened)(state);
+    //   var iso = date.toISOString()(state);
+    //   return iso;
+    // }),
     field('Case_Closed_Date__c', state => {
       var closed = dataValue('date_closed')(state);
       var date = dataValue('server_modified_on')(state);
