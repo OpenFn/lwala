680384fc5664c4f6c28ae36653c79a95324a4934
Taylor Downs <taylor@openfn.org>
2022-10-26 09:32:13 +0100

    hngondoki@lwalacommunityalliance.org's change on OpenFn.org

--- bulk/bulk-fetch.js
+++ bulk/bulk-fetch.js
@@ -10,8 +10,8 @@ fn(state => {
   ];
 
   const limit = 1000;
-  const indexedOnStart = '2022-06-30';
-  const receivedOnEnd = '2022-06-15';
+  const indexedOnStart = '2022-06-14';
+  const receivedOnEnd = '2022-06-01';
 
   const queries = formIds.map(
     id =>
