df85adf15fc758eb30b2e56ccc174d243ccc26cd
mtuchi <mtuchidev@gmail.com>
2023-04-18 15:38:53 +0300

    add recursive get for household case type

--- bulk/getCases-HH.js
+++ bulk/getCases-HH.js
@@ -1,26 +1,15 @@
-get(`${state.configuration.baseUrl}`, {
-  query: `?type=Household&indexed_on_start=${state.indexed_on_start}&limit=5000`,
-  headers: { 'content-type': 'application/json' },
-});
-
 fn(state => {
-  const baseUrl =
-    'https://www.commcarehq.org/a/lwala-community-alliance/api/v0.5/case/';
+  const { baseUrl } = state.configuration;
 
   const caseTypes = ['Household'];
 
-  const limit = 1000;
-  const indexedOnStart = '2023-01-01';
-  const receivedOnEnd = '2023-01-01';
-  // const indexedOnStart = '2022-05-31';
-  // const receivedOnEnd = '2022-05-01';
+  const limit = 5000;
+  const indexedOnStart = '2023-01-01T00:00:00';
+  const lastRunOn =
+    typeof state.lastRunOn !== 'undefined' ? state.lastRunOn : indexedOnStart;
 
   const queries = caseTypes.map(
-    t =>
-      `?type=${t}` +
-      `?indexed_on_start=${indexedOnStart}` +
-      // `&received_on_end=${receivedOnEnd}` +
-      `&limit=${limit}`
+    t => `?type=${t}&indexed_on_start=${lastRunOn}&limit=${limit}`
   );
 
   return { ...state, queries, baseUrl, payloads: [] };
@@ -30,30 +19,44 @@ fn(state => {
 // more data to fetch for the same form
 fn(state => {
   const recursiveGet = url =>
-    get(url, {}, nextState => {
-      const { baseUrl, data, payloads } = nextState;
-      const { meta, objects } = data;
-      console.log('Metadata in CommCare response:', meta);
+    get(
+      url,
+      {
+        headers: { 'content-type': 'application/json' },
+      },
+      nextState => {
+        const now = new Date();
+        const { baseUrl, data, payloads } = nextState;
 
-      const finalState = { ...nextState, payloads: [...payloads, ...objects] };
+        const { meta, objects } = data;
+        console.log('Metadata in CommCare response:', meta);
 
-      if (meta.next) {
-        console.log('Next query detected, recursing...');
-        return recursiveGet(`${baseUrl}${meta.next}`)(finalState);
+        const finalState = {
+          ...nextState,
+          payloads: [...payloads, ...objects],
+        };
+
+        if (meta.next) {
+          console.log('Next query detected, recursing...');
+          return recursiveGet(`${baseUrl}${meta.next}`)(finalState);
+        }
+        finalState.lastRunOn = now.toISOString().slice(0, 19);
+        return finalState;
       }
-      return finalState;
-    });
+    );
 
   return { ...state, recursiveGet };
 });
 
-// // for each initial query, fetch data recursively
-// each(
-//   '$.queries[*]',
-//   fn(state => state.recursiveGet(`${state.baseUrl}${state.data}`)(state))
-// );
-// // log the total number of payloads returned
-// fn(state => {
-//   console.log('Count of payloads', state.payloads.length);
-//   return { ...state, references: [], data: {} };
-// });
+// for each initial query, fetch data recursively
+each(
+  '$.queries[*]',
+  fn(state => {
+    return state.recursiveGet(`${state.baseUrl}${state.data}`)(state);
+  })
+);
+// log the total number of payloads returned
+fn(state => {
+  console.log('Count of payloads', state.payloads.length);
+  return { ...state, references: [], data: {} };
+});
