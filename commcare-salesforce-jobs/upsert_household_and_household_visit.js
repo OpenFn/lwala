679660f23b95695cb5f2ec1085a306205c5df474
Aleksa Krolls <aleksagk@gmail.com>
2023-04-19 09:47:23 +0300

    mtuchi@openfn.org's change on OpenFn.org

--- commcare-salesforce-jobs/upsert_household_and_household_visit.js
+++ commcare-salesforce-jobs/upsert_household_and_household_visit.js
@@ -1,5 +1,5 @@
 fn(state => {
-  const owner_ids = state.paylods.map(data => data.properties.owner_id);
+  const owner_ids = state.payloads.map(data => data.properties.owner_id);
   const uniq_owner_ids = [...new Set(owner_ids)];
 
   return { ...state, uniq_owner_ids };
@@ -56,7 +56,7 @@ fn(state => {
     none: 'None',
   };
 
-  const households = state.paylods
+  const households = state.payloads
     .filter(
       h =>
         h.properties.commcare_username !== 'openfn.test' &&
@@ -144,7 +144,7 @@ fn(state => {
       };
     });
 
-  const housevisits = state.paylods
+  const housevisits = state.payloads
     .filter(
       h =>
         h.properties.commcare_username !== 'openfn.test' &&
