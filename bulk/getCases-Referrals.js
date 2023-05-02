10a649defe4872f75b90a809d69ca9c70edcebaa
Aleksa Krolls <aleksagk@gmail.com>
2023-05-02 14:04:56 +0200

    aleksa@openfn.org's change on OpenFn.org

--- bulk/getCases-Referrals.js
+++ bulk/getCases-Referrals.js
@@ -18,7 +18,7 @@ fn(state => {
     //NOTE: If for testing, you want to fetch data for a specific historical range (e.g., between April 23 and 24)...
     //...then use the query string below instead of the one above on L16, and custom adjust the index_on start/end dates
     t =>
-      `?type=${t}&indexed_on_start=2023-03-01T00:00:00&limit=${limit}&indexed_on_end=2023-03-31T00:00:00`
+      `?type=${t}&indexed_on_start=2023-03-01T00:00:00&limit=${limit}&indexed_on_end=2023-03-02T00:00:00`
   );
 
   return { ...state, queries, baseUrl, payloads: [] };
