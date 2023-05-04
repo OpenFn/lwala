9d2f26dc9a08f7b21019578dbdaf304941350b03
Aleksa Krolls <aleksagk@gmail.com>
2023-05-04 11:25:36 +0200

    aleksa@openfn.org's change on OpenFn.org

--- bulk/getCases-HH.js
+++ bulk/getCases-HH.js
@@ -4,7 +4,7 @@ fn(state => {
   const caseTypes = ['Household'];
 
   //NOTE: You can use 'limit' to modify this batch size as desired
-  const limit = 10; //E.g., change to 10 to process records in smaller batch sizes
+  const limit = 1000; //E.g., change to 10 to process records in smaller batch sizes
 
   //NOTE: indexedOnStart is the default sync start data the FIRST time the job runs
   const indexedOnStart = '2023-03-01T00:00:00';
@@ -17,7 +17,8 @@ fn(state => {
     //t => `?type=${t}&indexed_on_start=${lastRunAt}&limit=${limit}`
     //NOTE: If for testing, you want to fetch data for a specific historical range (e.g., between April 23 and 24)...
     //...then use the query string below instead of the one above on L16, and custom adjust the index_on start/end dates
-    t => `?type=${t}&indexed_on_start=2023-03-01T00:00:00&limit=${limit}&indexed_on_end=2023-03-03T00:00:00`
+    //t => `?type=${t}&indexed_on_start=2023-03-01T00:00:00&limit=${limit}&indexed_on_end=2023-03-03T00:00:00` //returns 14 records
+    t => `?type=${t}&indexed_on_start=2023-03-01T00:00:00&limit=${limit}&indexed_on_end=2023-03-01T22:00:00`
   );
 
   return { ...state, queries, baseUrl, payloads: [] };
