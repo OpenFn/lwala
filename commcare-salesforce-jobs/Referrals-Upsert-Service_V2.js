7eb5180d01fbbc32f886247ddd3da2d3fb3d3e84
Aleksa Krolls <aleksagk@gmail.com>
2023-05-26 09:11:42 +0200

    hngondoki@lwalacommunityalliance.org's change on OpenFn.org

--- commcare-salesforce-jobs/Referrals-Upsert-Service_V2.js
+++ commcare-salesforce-jobs/Referrals-Upsert-Service_V2.js
@@ -376,11 +376,11 @@ fn(state => {
         //Person_Complied_w_Referral_in_24_hrs__c:
          // r.properties.referral_compliance,
          //HMN commented Skillled_Delivery__c,Child_received_immunizations__c
-         //Received_a_diagnosis_for_PSBI__c
+         //Received_a_diagnosis_for_PSBI__c,Received_antibiotics_per_protocol__c
        // Skillled_Delivery__c: r.properties.skilled_delivery,
         //Child_received_immunizations__c: r.properties.immunization,
         //Received_a_diagnosis_for_PSBI__c: r.properties.psbi_diagnosis, //Form: CHW.Follow-Up.PSBI.psbi_diagnosis
-        Received_antibiotics_per_protocol__c: r.properties.antibiotic_8days, //Form: CHW.Follow-Up.PSBI.antibiotic_8day
+        //Received_antibiotics_per_protocol__c: r.properties.antibiotic_8days, //Form: CHW.Follow-Up.PSBI.antibiotic_8day
         Distributed_Treatment_on_Last_Visit__c:
           r.properties.distribute_treatment, //Form: CHW.Follow-Up.distribute_treatment
         Person_had_an_adverse_drug_reaction__c:
