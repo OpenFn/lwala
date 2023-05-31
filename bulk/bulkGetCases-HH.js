8740086fdae6b48f9a24ea5b7294a2957a58c976
Taylor Downs <taylor@openfn.org>
2023-05-31 16:21:47 +0300

    hngondoki@lwalacommunityalliance.org's change on OpenFn.org

--- bulk/bulkGetCases-HH.js
+++ bulk/bulkGetCases-HH.js
@@ -19,7 +19,7 @@ fn(state => {
     //NOTE: If for testing, you want to fetch data for a specific historical range (e.g., between April 23 and 24)...
     //...then use the query string below instead of the one above on L16, and custom adjust the index_on start/end dates
     //t => `?type=${t}&indexed_on_start=2023-03-03T00:00:00&limit=${limit}&indexed_on_end=2023-03-06T00:00:00` //returns 14 records
-    t => `?type=${t}&indexed_on_start=2023-05-31T15:45:00&limit=${limit}&indexed_on_end=2023-05-31T15:50:00` 
+    t => `?type=${t}&indexed_on_start=2023-05-31T15:50:00&limit=${limit}&indexed_on_end=2023-05-31T15:55:00` 
   );
 
   return { ...state, queries, baseUrl, payloads: [] };
