ca9d12feb074aa73dda2bf577a44478eefaf442f
Taylor Downs <taylor@openfn.org>
2023-10-23 12:58:00 +0200

    hngondoki@lwalacommunityalliance.org's change on OpenFn.org

--- bulk/bulkGetCases-HH.js
+++ bulk/bulkGetCases-HH.js
@@ -7,7 +7,7 @@ fn(state => {
   const limit = 1000; 
 
   //NOTE: indexedOnStart is the default sync start data the FIRST time the job runs
-  const indexedOnStart = '2023-08-09T21:00:00';
+  const indexedOnStart = '2023-10-22T11:00:00';
 
   //NOTE: After first job run, OpenFn will check the job sync data ("lastRunAt") to set as the indexedOnStart
   const lastRunAt =
