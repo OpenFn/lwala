d25d6ecd7d668ef11f443f8ba40a36218d9a4e57
Taylor Downs <taylor@openfn.org>
2024-01-18 12:20:08 +0000

    hngondoki@lwalacommunityalliance.org's change on OpenFn.org

--- bulk/bulkUpsert-HHandHHVisit.js
+++ bulk/bulkUpsert-HHandHHVisit.js
@@ -160,7 +160,7 @@ fn(state => {
           h.properties.when_was_the_household_provided_with_seed_input_support,
         MIYCN_Trained__c: h.properties.household_trained_on_MIYC,
         // TODO: @Aleksa to find out if Case_Closed_Date__c still exist
-         Case_Closed_Date__c: h.date_closed && h.date_closed == true
+         Case_Closed_Date__c: h.date_closed //&& h.date_closed == true
              ? h.date_closed
              : undefined 
       };
